4d57cf987af49ddf7f6c4927047bb8f6919f2dd1
julpereira <julia.p@estudante.ifro.edu.br>
2024-08-22 11:24:34 -0400

    back

--- projeto/index.js
+++ projeto/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get ('/',(req, res) => {
-    res.send('Hello World!');
+    let mensagem = ({mensagem:'Olá, Mundo!'});
+    res.json(mensagem);
 });
 
 app.listen(8080, () => {
